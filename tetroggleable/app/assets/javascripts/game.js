8779225a6b51db86a5a06f6c5978a69f04ca9108
John Berry <jpberry0623@gmail.com>
2014-09-04 18:48:14 -0400

    add game.js file
    -add document ready function
    -declare context, row, column variables

---
+++ tetroggleable/app/assets/javascripts/game.js
@@ -0,0 +1,19 @@
+var context;
+var ROWS = 20;
+var COLS = 10;
+var SIZE = 32;
+
+var canvas;
+var lineScore;
+
+
+$(document).ready(function(){
+
+
+	canvas = document.getElementById('gameCanvas');
+	context = canvas.getContext('2d');
+	lineScore = $('#lines');
+	
+	
+})
+
