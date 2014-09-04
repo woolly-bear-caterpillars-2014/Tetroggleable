0f374981399a1389f4a5a511a07a497ec9a5d08c
John Berry <jpberry0623@gmail.com>
2014-09-04 19:07:17 -0400

    add game init function
    -instantiate new board
    -declare animation frame windows

--- tetroggleable/app/assets/javascripts/game.js
+++ tetroggleable/app/assets/javascripts/game.js
@@ -1,10 +1,11 @@
-var context;
 var ROWS = 20;
 var COLS = 10;
 var SIZE = 32;
 
 var canvas;
+var context;
 var lineScore;
+var currentPiece;
 
 
 $(document).ready(function(){
@@ -14,6 +15,35 @@ $(document).ready(function(){
 	context = canvas.getContext('2d');
 	lineScore = $('#lines');
 	
+	function initGame()
+{	
 	
+	var r, c;
+	currentLines = 0;
+	isGameOver = false;
+	
+	gameData = new Array();
+		
+		for(r = 0; r < ROWS; r++)
+		{
+			gameData[r] = new Array();
+			for(c = 0; c < COLS; c++)
+			{
+				// gameData[r].push(0);
+				gameData[r][c] = 0;
+			}
+		}	
+
+	currentPiece = getRandomPiece();
+	
+
+	
+	var requestAnimFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+			window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+							
+	window.requestAnimationFrame = requestAnimFrame;
+	
+	requestAnimationFrame(update);
+	}
 })
 
