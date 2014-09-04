fa7eba016b78db2e97426edbb817127c6ca5b7a7
eddiekimosabe <eddiekimosabe@gmail.com>
2014-09-04 19:30:23 -0400

    Add getKeyCode function
    -Create switch statement to list key down events
    -List key codes 37-40 to indicate arrow movements of blocks

--- tetroggleable/app/assets/javascripts/game.js
+++ tetroggleable/app/assets/javascripts/game.js
@@ -4,46 +4,80 @@ var SIZE = 32;
 
 var canvas;
 var context;
+var currentBlock;
 var lineScore;
-var currentPiece;
-
 
 $(document).ready(function(){
-
 
 	canvas = document.getElementById('gameCanvas');
 	context = canvas.getContext('2d');
 	lineScore = $('#lines');
-	
-	function initGame()
-{	
-	
-	var r, c;
+
+	function initGame() {
+
+	var row, col;
 	currentLines = 0;
 	isGameOver = false;
-	
+
 	gameData = new Array();
-		
-		for(r = 0; r < ROWS; r++)
-		{
-			gameData[r] = new Array();
-			for(c = 0; c < COLS; c++)
-			{
+
+		for(row= 0; row < ROWS; row++) {
+			gameData[row] = new Array();
+			for(col = 0; col < COLS; col++) {
 				// gameData[r].push(0);
-				gameData[r][c] = 0;
+				gameData[row][col] = 0;
 			}
-		}	
+		}
 
-	currentPiece = getRandomPiece();
-	
+	currentBlock = getRandomPiece();
 
-	
 	var requestAnimFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
 			window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
-							
+
 	window.requestAnimationFrame = requestAnimFrame;
-	
+
 	requestAnimationFrame(update);
 	}
-})
+});
+
+function getKeyCode(e) {
+	// if(!e) { var e = window.event; }
+
+	e.preventDefault();
+
+	if(isGameOver != true) {
+		switch(e.keyCode) {
+			case 37: {
+				if( checkMove(currentBlock.gridX - 1, currentBlock.gridY, currentBlock.currentRotation) )
+					currentBlock.gridX--;
+			}
+			break;
+
+			case 39: {
+				if( checkMove(currentBlock.gridX + 1, currentBlock.gridY, currentBlock.currentRotation) )
+					currentBlock.gridX++;
+			}
+			break;
+
+			case 38: {
+				var newRotation = currentBlock.currentRotation - 1;
+				if(newRotation < 0)
+					newRotation = currentBlock.rotations.length - 1;
+
+				if( checkMove(currentBlock.gridX, currentBlock.gridY, newRotation) )
+					currentBlock.currentRotation = newRotation;
+			}
+			break;
+
+			case 40: {
+				if( checkMove(currentBlock.gridX, currentBlock.gridY + 1, currentBlock.currentRotation) )
+					currentBlock.gridY++;
+			}
+			break;
+		}
+	}
+	else {
+		initGame();
+	}
+}
 
