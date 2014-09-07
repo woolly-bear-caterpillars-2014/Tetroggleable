df257250d28cec03bb0b95a0790e3cef0b74e7b6
Stephen Estrada <stcrestrada@gmail.com>
2014-09-07 12:35:35 -0400

    create user controller actions: -new user -create user -create strong params
    create partials form in user views
    renders partial form in user new view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,38 @@
 class UsersController < ApplicationController
 	
 	def index
+		@users = User.all
 	end
 	
 	def show
 	end
 	
 	def new
+		@user = User.new
 	end
 
 	def edit
 	end
 
 	def create
+		@user = User.new(user_params)
+		if @user.save
+			redirect_to applications_path
+		else
+			render :new
+		end
 	end
 
 	def update
 	end
 
 	def destroy
+	end
+
+	private 
+
+	def user_params
+		params.require(:user).permit(:username)
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'application#home'
+   root 'users#index'
+   resources :users
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
