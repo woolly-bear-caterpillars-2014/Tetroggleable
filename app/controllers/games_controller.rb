0ba9423eeded6f53f2c127389d90dbcef57545c5
Stephen Estrada <stcrestrada@gmail.com>
2014-09-07 12:56:23 -0400

    create game controller actions: -index action -new action -create action
    create game index view

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,27 @@
+class GamesController < ApplicationController
+	def index
+		
+	end
+	
+	
+	def new
+		@game = Game.new
+	end
+
+	def create
+		@game = Game.new(params[:game])
+		if @game.save
+			redirect_to games_path
+		else
+			render :new
+		end
+	end
+
+
+	# private 
+
+	# def game_params
+	# 	params.require(:game).permit(:score, :scrabble_score, :level, :lines)
+	# end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'users#index'
+   
+   root 'users#new'
    resources :users
-
+   resources :applications
+   resources :games
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
