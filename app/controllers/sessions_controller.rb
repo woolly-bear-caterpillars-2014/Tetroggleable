f9500b2e27bceeecd0a05a46b7251df9f75a31de
Stephen Estrada <stcrestrada@gmail.com>
2014-09-08 18:20:05 -0400

    add content_tag into layouts/application.html.erb -display error in login if user validation is incorrect
    Add logic in create action in game controller.
    -if user login information is incorrect display error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id] = @user.id
       redirect_to new_game_path
     else
+      flash.now[:notice] = "Please enter valid username or password!"
       render :new
     end
 
