89bc4a5efc3478d97fdf6e78ef93deb4d9d4af8d
Stephen Estrada <stcrestrada@gmail.com>
2014-09-09 15:17:51 -0400

    Add create test to userscontroller spec -create user when valid params are passed -rspec success

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -22,4 +22,12 @@ describe UsersController do
 				expect(assigns(:user)).to be_a User
 			end
 		end
+
+		describe "POST #create" do 
+			context "when valids params are passed" do 
+				it "creates a new user" do 
+					post :create, user: FactoryGirl.attributes_for(:user)
+				end
+			end
+		end
 	end
