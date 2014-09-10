1b0cbbe87849c94dadf4b6c060cacbddaa50acab
Stephen Estrada <stcrestrada@gmail.com>
2014-09-10 09:23:39 -0400

    Create game validations in game model validate presence of: -score -scrabble score -level -lines

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,9 @@
 class Game < ActiveRecord::Base
 	belongs_to :user
 
+	validates :score, presense: true
+	validates :scrabble_score, presense: true
+	validates :level, presense: true
+	validates :lines, presense: true
+
 end
