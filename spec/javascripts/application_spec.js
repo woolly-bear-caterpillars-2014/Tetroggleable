4be8130eb437260f714c8db106752653b5eca80b
John Berry <jpberry0623@gmail.com>
2014-09-10 12:56:02 -0400

    Add ZBlock test

--- spec/javascripts/application_spec.js
+++ spec/javascripts/application_spec.js
@@ -35,27 +35,27 @@ describe ("LBlock", function () {
     lBlock = new LBlock(tileArray);
     });
 
-     it("should return an LBlock rotation1", function() {
+     it("should return the correct tile letter for LBlock rotation1", function() {
 
       expect(lBlock.rotation1).toBeDefined();
       expect(lBlock.rotation1[0][0].letter).toEqual('D')
      });
 
-     it("should return an LBlock rotation2", function() {
+     it("should return the correct tile letter for LBlock rotation2", function() {
 
       expect(lBlock.rotation2).toBeDefined();
       expect(lBlock.rotation2[0][2].letter).toEqual('A')
 
      });
 
-     it("should return an LBlock rotation3", function() {
+     it("should return the correct tile letter for LBlock rotation3", function() {
 
       expect(lBlock.rotation3).toBeDefined();
       expect(lBlock.rotation3[0][1].letter).toEqual('B')
 
      });
 
-     it("should return an LBlock rotation4", function() {
+     it("should return the correct tile letter for LBlock rotation4", function() {
 
       expect(lBlock.rotation4).toBeDefined();
       expect(lBlock.rotation4[0][1].letter).toEqual('C')
@@ -79,27 +79,27 @@ describe ("JBlock", function () {
     jBlock = new JBlock(tileArray);
     });
 
-     it("should return an JBlock rotation1", function() {
+     it("should return the correct tile letter for JBlock rotation1", function() {
 
       expect(jBlock.rotation1).toBeDefined();
       expect(jBlock.rotation1[0][1].letter).toEqual('A')
      });
 
-     it("should return an JBlock rotation2", function() {
+     it("should return the correct tile letter for JBlock rotation2", function() {
 
       expect(jBlock.rotation2).toBeDefined();
       expect(jBlock.rotation2[0][2].letter).toEqual('C')
 
      });
 
-     it("should return an JBlock rotation3", function() {
+     it("should return the correct tile letter for JBlock rotation3", function() {
 
       expect(jBlock.rotation3).toBeDefined();
       expect(jBlock.rotation3[0][1].letter).toEqual('B')
 
      });
 
-     it("should return an JBlock rotation4", function() {
+     it("should return the correct tile letter for JBlock rotation4", function() {
 
       expect(jBlock.rotation4).toBeDefined();
       expect(jBlock.rotation4[1][1].letter).toEqual('D')
@@ -123,27 +123,27 @@ describe ("IBlock", function () {
     iBlock = new IBlock(tileArray);
     });
 
-     it("should return an iBlock rotation1", function() {
+     it("should return the correct tile letter for iBlock rotation1", function() {
 
       expect(iBlock.rotation1).toBeDefined();
       expect(iBlock.rotation1[0][0].letter).toEqual('A')
      });
 
-     it("should return an iBlock rotation2", function() {
+     it("should return the correct tile letter for iBlock rotation2", function() {
 
       expect(iBlock.rotation2).toBeDefined();
       expect(iBlock.rotation2[0][2].letter).toEqual('C')
 
      });
 
-     it("should return an iBlock rotation3", function() {
+     it("should return the correct tile letter for iBlock rotation3", function() {
 
       expect(iBlock.rotation3).toBeDefined();
       expect(iBlock.rotation3[0][0].letter).toEqual('D')
 
      });
 
-     it("should return an iBlock rotation4", function() {
+     it("should return the correct tile letter for iBlock rotation4", function() {
 
       expect(iBlock.rotation4).toBeDefined();
       expect(iBlock.rotation4[0][2].letter).toEqual('B')
@@ -167,27 +167,27 @@ describe ("TBlock", function () {
     tBlock = new TBlock(tileArray);
     });
 
-     it("should return an TBlock rotation1", function() {
+     it("should return the correct tile letter for TBlock rotation1", function() {
 
       expect(tBlock.rotation1).toBeDefined();
       expect(tBlock.rotation1[1][1].letter).toEqual('A')
      });
 
-     it("should return an TBlock rotation2", function() {
+     it("should return the correct tile letter for TBlock rotation2", function() {
 
       expect(tBlock.rotation2).toBeDefined();
       expect(tBlock.rotation2[0][0].letter).toEqual('B')
 
      });
 
-     it("should return an TBlock rotation3", function() {
+     it("should return the correct tile letter for TBlock rotation3", function() {
 
       expect(tBlock.rotation3).toBeDefined();
       expect(tBlock.rotation3[1][1].letter).toEqual('C')
 
      });
 
-     it("should return an TBlock rotation4", function() {
+     it("should return the correct tile letter for TBlock rotation4", function() {
 
       expect(tBlock.rotation4).toBeDefined();
       expect(tBlock.rotation4[0][1].letter).toEqual('D')
@@ -196,3 +196,31 @@ describe ("TBlock", function () {
     });
 });
 
+describe ("ZBlock", function () {
+  beforeEach(function () {
+
+    tile1 = new Tile();
+    tile1.letter = "A";
+    tile2 = new Tile();
+    tile2.letter = "B";
+    tile3 = new Tile();
+    tile3.letter = "C";
+    tile4 = new Tile();
+    tile4.letter = "D";
+    var tileArray = [tile1, tile2, tile3, tile4];
+    zBlock = new ZBlock(tileArray);
+    });
+
+     it("should return the correct tile letter for ZBlock rotation1", function() {
+
+      expect(zBlock.rotation1).toBeDefined();
+      expect(zBlock.rotation1[1][2].letter).toEqual('A')
+     });
+
+     it("should return the correct tile letter for ZBlock rotation2", function() {
+
+      expect(zBlock.rotation2).toBeDefined();
+      expect(zBlock.rotation2[1][0].letter).toEqual('B')
+
+    });
+});
