480bc7d62b259c4b46a419825320c27ad1967b00
John Berry <jpberry0623@gmail.com>
2014-09-10 12:47:30 -0400

    add tests for I and J block rotations

--- spec/javascripts/application_spec.js
+++ spec/javascripts/application_spec.js
@@ -66,61 +66,91 @@ describe ("LBlock", function () {
 
 describe ("JBlock", function () {
   beforeEach(function () {
-    jBlock = new JBlock([]);
+
+    tile1 = new Tile();
+    tile1.letter = "A";
+    tile2 = new Tile();
+    tile2.letter = "B";
+    tile3 = new Tile();
+    tile3.letter = "C";
+    tile4 = new Tile();
+    tile4.letter = "D";
+    var tileArray = [tile1, tile2, tile3, tile4];
+    jBlock = new JBlock(tileArray);
     });
 
-     it("should return an JBlock rotation object", function() {
+     it("should return an JBlock rotation1", function() {
 
       expect(jBlock.rotation1).toBeDefined();
+      expect(jBlock.rotation1[0][1].letter).toEqual('A')
      });
 
-     it("should return an JBlock rotation object", function() {
+     it("should return an JBlock rotation2", function() {
 
       expect(jBlock.rotation2).toBeDefined();
+      expect(jBlock.rotation2[0][2].letter).toEqual('C')
 
      });
 
-     it("should return an JBlock rotation object", function() {
+     it("should return an JBlock rotation3", function() {
 
       expect(jBlock.rotation3).toBeDefined();
+      expect(jBlock.rotation3[0][1].letter).toEqual('B')
 
      });
 
-     it("should return an JBlock rotation object", function() {
+     it("should return an JBlock rotation4", function() {
 
       expect(jBlock.rotation4).toBeDefined();
+      expect(jBlock.rotation4[1][1].letter).toEqual('D')
+
 
     });
 });
 
-describe ("OBlock", function () {
+describe ("IBlock", function () {
   beforeEach(function () {
-    oBlock = new OBlock([]);
+
+    tile1 = new Tile();
+    tile1.letter = "A";
+    tile2 = new Tile();
+    tile2.letter = "B";
+    tile3 = new Tile();
+    tile3.letter = "C";
+    tile4 = new Tile();
+    tile4.letter = "D";
+    var tileArray = [tile1, tile2, tile3, tile4];
+    iBlock = new IBlock(tileArray);
     });
 
-     it("should return an OBlock rotation object", function() {
+     it("should return an iBlock rotation1", function() {
 
-      expect(oBlock.rotation1).toBeDefined();
+      expect(iBlock.rotation1).toBeDefined();
+      expect(iBlock.rotation1[0][0].letter).toEqual('A')
      });
 
-     it("should return an OBlock rotation object", function() {
+     it("should return an iBlock rotation2", function() {
 
-      expect(oBlock.rotation2).toBeDefined();
-
-     });
-
-     it("should return an OBlock rotation object", function() {
-
-      expect(oBlock.rotation3).toBeDefined();
+      expect(iBlock.rotation2).toBeDefined();
+      expect(iBlock.rotation2[0][2].letter).toEqual('C')
 
      });
 
-     it("should return an OBlock rotation object", function() {
+     it("should return an iBlock rotation3", function() {
 
-      expect(oBlock.rotation4).toBeDefined();
+      expect(iBlock.rotation3).toBeDefined();
+      expect(iBlock.rotation3[0][0].letter).toEqual('D')
 
      });
-  });
+
+     it("should return an iBlock rotation4", function() {
+
+      expect(iBlock.rotation4).toBeDefined();
+      expect(iBlock.rotation4[0][2].letter).toEqual('B')
+
+
+    });
+});
 
 describe ("TBlock", function () {
   beforeEach(function () {
@@ -151,12 +181,3 @@ describe ("TBlock", function () {
      });
   });
 
-// describe("startGame", function () {
-//   beforeEach(function () {
-//     var start = new startGame();
-//   });
-
-//     it("should be defined", function () {
-//       expect(drawPreview()).toHaveBeenCalled();
-//     })
-// })
